62cad739e9194c25ab5e996fa67de806e6d376a0
Jason Steffler <steffler.jason@gmail.com>
2014-02-13 15:36:44 -0500

    Created a rails application and added statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtMentors::Application.config.secret_key_base = '296f6420047818209d5598f843b61c48cb7ef202be50e6db273af3a7062c4cb1cf5108c92558cd94f54bb880411536dd374ef51a9402254d7fefc889924204fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArtMentors::Application.config.session_store :cookie_store, key: '_ArtMentors_session'
